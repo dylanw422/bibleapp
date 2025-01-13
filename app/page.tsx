3b2e40a824758a0a991fb7279dd4ec7d2db96cdc
dylanw422 <dylanwestdev@gmail.com>
2025-01-13 15:46:46 -0600

    style changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { SignedOut, SignInButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
@@ -10,7 +11,11 @@ export default function Home() {
       <h1 className="text-4xl my-4 font-bold">Bible App</h1>
       <div className="space-x-4">
         <Button onClick={() => router.push("/bible/kjv/1")}>Get Started</Button>
-        <Button>Info</Button>
+        <SignedOut>
+          <SignInButton>
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
       </div>
     </div>
   );
