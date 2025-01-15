bb974360f4dfbe83d092f7c47664ddec536ce19a
dylanw422 <dylanwestdev@gmail.com>
2025-01-15 09:23:55 -0600

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { Header } from "@/components/landing/header";
+import { Hero } from "@/components/landing/hero";
 import { Button } from "@/components/ui/button";
 import { SignedOut, SignInButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
@@ -7,16 +9,9 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl my-4 font-bold">Bible App</h1>
-      <div className="space-x-4">
-        <Button onClick={() => router.push("/bible/kjv/1")}>Get Started</Button>
-        <SignedOut>
-          <SignInButton>
-            <Button>Sign In</Button>
-          </SignInButton>
-        </SignedOut>
-      </div>
+    <div className="flex flex-col items-center min-h-screen">
+      <Header />
+      <Hero />
     </div>
   );
 }
