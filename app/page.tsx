3be72c29fc5877c1b21f7b8553af3eb27c456e78
dylanw422 <dylanwestdev@gmail.com>
2025-01-15 17:19:30 -0600

    build error fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 import { Header } from "@/components/landing/header";
 import { Hero } from "@/components/landing/hero";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
   return (
     <div className="flex flex-col items-center min-h-screen">
       <Header />
