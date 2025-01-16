3df2e4daeed1481c1128a01337b6e16b702c84a5
dylanw422 <dylanwestdev@gmail.com>
2025-01-16 09:24:50 -0600

    clerk key

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,9 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
       <QueryProvider>
         <html lang="en" suppressHydrationWarning>
           <body
